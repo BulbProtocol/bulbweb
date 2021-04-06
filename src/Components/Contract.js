3b6ee5ffb58731cfcc74438ce10e8daa0f29985c
Sebastian Bainotti <info@bulbprotocol.com>
2021-04-06 19:22:01 -0300

    changes

--- src/Components/Contract.js
+++ src/Components/Contract.js
@@ -10,9 +10,11 @@ class Contract extends Component {
             <img src="/images/bulb2.jpg" alt="bulb " />
           </div>
           <div class="nine columns main-col">
-            <span>BULB is a deflationary dividend generating protocol. Buying and holding BULB means you are passively staking your tokens while receiving frictionless payments for each transaction made.</span>
-            
-
+            <span>BULB is a deflationary Auto-Stake protocol.</span>
+            <span className="title">When you hold $BULB, you will always be rewarded. </span>
+            <span className="desc">This means you earn more tokens just for keeping $ BULB in your wallet, when someone makes a transaction (buy or sell) 2% is distributed to all holders.</span>
+            <span className="title">Long-term stable. </span>
+            <span className="desc">The deflationary protocol is in the long run one of the greatest advantages, since with each operation, 2% is also burned, fewer tokens in circulation, greater value of the token.</span>
           </div>
         </div>
       </section>
