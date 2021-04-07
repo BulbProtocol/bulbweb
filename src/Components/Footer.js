866ee767b9dd0ee8c4516a812794bb4e335e5f43
Sebastian Bainotti <info@bulbprotocol.com>
2021-04-07 12:11:14 -0300

    sss

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,13 +8,14 @@ class Footer extends Component {
           <div className="twelve columns">
             <ul className="social-links">
               <li><a href="https://twitter.com/BulbProtocol" alt="Twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="" alt="Telegram"><img src="/images/telegram.png" alt="" target="_blank" /></a></li>
-              <li><a href="https://medium.com/@BulbProtocol" alt="Medium" target="_blank"><img src="/images/medium.png" alt="" /></a></li>
+              <li><a href="https://telegram.com/BulbProtocol" alt="Twitter" target="_blank"><i class="fab fa-telegram-plane"></i></a></li>
+              <li><a href="https://medium.com/@BulbProtocol" alt="Twitter" target="_blank"><i class="fab fa-medium-m"></i></a></li>
+
             </ul>
 
             <ul className="copyright">
               <li>&copy; Copyright 2021 BULB Protocol</li>
-              
+
             </ul>
           </div>
           <div id="go-top">
